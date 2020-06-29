eab4802634dafa867d61fb4e957129fa1945cc35
kkimtae <kkimtae@gmail.com>
2020-06-30 08:07:13 +0900

    change

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,11 +1,37 @@
-import * as React from "react";
-import { View, Text, Button } from "react-native";
+import React from "react";
+import axios from "axios";
+import { View, Text, TouchableOpacity } from "react-native";
 
-function HomeScreen({ navigation }) {
+const seoulApiKey = "56566d4f7a6b6b69333572706d676d";
+
+async function CheckNanoDust() {
+  const {
+    data: {
+      ListAirQualityByDistrictService: { row },
+    },
+  } = await axios.get(
+    `http://openapi.seoul.go.kr:8088/${seoulApiKey}/json/ListAirQualityByDistrictService/1/50`
+  );
+}
+
+function HomeScreen({ navigation, route }) {
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>Home Screen</Text>
-      <Button title="Profile" onPress={() => navigation.navigate("Profile")} />
+      <Text
+        style={{ alignItems: "center", justifyContent: "center", fontSize: 30 }}
+      >
+        서울시 미세먼지 정보
+      </Text>
+      <TouchableOpacity onPress={() => navigation.navigate("Profile")}>
+        <Text style={{ fontSize: 25, margin: 10, marginTop: 30 }}>
+          지역설정
+        </Text>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={CheckNanoDust}>
+        <Text style={{ fontSize: 25, margin: 10 }}>
+          {route.params?.selectDistrict.label} 조회
+        </Text>
+      </TouchableOpacity>
     </View>
   );
 }
--- screens/ProfileScreen.js
+++ screens/ProfileScreen.js
@@ -1,13 +1,84 @@
 import * as React from "react";
-import { View, Text, Button } from "react-native";
+import { View, Text } from "react-native";
+import { TouchableOpacity } from "react-native-gesture-handler";
+import DropDownPicker from "react-native-dropdown-picker";
 
 function ProfileScreen({ navigation }) {
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>Profile Screen</Text>
-      <Button title="Modal" onPress={() => navigation.navigate("Modal")} />
+      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+        <DropDownPicker
+          items={districtList}
+          containerStyle={{
+            flex: 1,
+            flexDirection: "row",
+            justifyContent: "space-around",
+            margin: 20,
+          }}
+          style={{ backgroundColor: "#fafafa", height: 40 }}
+          dropDownStyle={{ backgroundColor: "#fafafa" }}
+          onChangeItem={(item) => {
+            navigation.navigate("Home", { selectDistrict: item });
+          }}
+          searchable={true}
+          searchablePlaceholder="지역 검색"
+          searchableError="서울 지역구만 선택 가능"
+          placeholder="지역 선택"
+          labelStyle={{ fontSize: 14 }}
+          placeholderStyle={{ fontWeight: "bold" }}
+          activeLabelStyle={{ color: "red" }}
+        />
+      </View>
+      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+        <TouchableOpacity
+          activeOpacity={0.6}
+          onPress={() => navigation.navigate("Modal")}
+          style={{
+            flex: 1,
+            backgroundColor: "#023e71",
+            justifyContent: "center",
+            alignItems: "center",
+            marginBottom: 10,
+            borderRadius: 5,
+            maxHeight: 40,
+            width: 200,
+          }}
+        >
+          <Text style={{ fontSize: 20, color: "white" }}>
+            Open Modal Screen
+          </Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
 
 export default ProfileScreen;
+
+const districtList = [
+  { key: "111123", value: "111123", label: "종로구" },
+  { key: "111121", value: "111121", label: "중구" },
+  { key: "111131", value: "111131", label: "용산구" },
+  { key: "111142", value: "111142", label: "성동구" },
+  { key: "111141", value: "111141", label: "광진구" },
+  { key: "111152", value: "111152", label: "동대문구" },
+  { key: "111151", value: "111151", label: "중랑구" },
+  { key: "111161", value: "111161", label: "성북구" },
+  { key: "111291", value: "111291", label: "강북구" },
+  { key: "111171", value: "111171", label: "도봉구" },
+  { key: "111311", value: "111311", label: "노원구" },
+  { key: "111181", value: "111181", label: "은평구" },
+  { key: "111191", value: "111191", label: "서대문구" },
+  { key: "111201", value: "111201", label: "마포구" },
+  { key: "111301", value: "111301", label: "양천구" },
+  { key: "111212", value: "111212", label: "강서구" },
+  { key: "111221", value: "111221", label: "구로구" },
+  { key: "111281", value: "111281", label: "금천구" },
+  { key: "111231", value: "111231", label: "영등포구" },
+  { key: "111241", value: "111241", label: "동작구" },
+  { key: "111251", value: "111251", label: "관악구" },
+  { key: "111262", value: "111262", label: "서초구" },
+  { key: "111261", value: "111261", label: "강남구" },
+  { key: "111273", value: "111273", label: "송파구" },
+  { key: "111274", value: "111274", label: "강동구" },
+];
